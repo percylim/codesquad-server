43f4f93f694f93295cb6c08f7e4174982de38762
Percy Lim <69828313+percylim@users.noreply.github.com>
2023-07-04 14:02:32 +0800

    app.js update

--- app.js
+++ app.js
@@ -3112,6 +3112,83 @@ app.post("/productData", function(req, res, next) {
 
                     });
                     });
+                    app.get("/invoiceTxnSearch", function(req, res, next) {
+                      var companyID = req.query.companyID;
+                      var supplierID = req.query.supplierID;
+                      var pur_sal = req.query.pur_sal;
+                      var invType = req.query.invType;
+                      var txnDate = req.query.txnDate;
+                      var dueDate = req.query.dueDate;
+
+                      console.log(companyID);
+                      var db = mysql.createConnection({
+                      host: process.env.DB_HOST,
+                      user: process.env.DB_USER,
+                      password: process.env.DB_PASSWORD,
+                      database: process.env.DB_NAME,
+                      timezone : "+00:00",
+                    });  // ale/  var userLevel = req.query.userLevel;
+
+                      var sql="SELECT * from invoiceTxn where companyID = '"+companyID+"' and suppCustID = '"+supplierID+"' and invType = '"+invType+"' and pur_sal = '"+pur_sal+"' and txnDate>='"+txnDate+"' and txnDate<='"+dueDate+"'";
+                        // console.log(req.beforeDestroy() {
+                       console.log(sql);
+                        // },);
+                      db.query(sql, function (err, results, fields) {
+                       if(err){
+                         console.log('Error while fetching invoiceTxn Record, err');
+                        // results(null,err);
+                        res.send(alert('fail to load invoiceTxn record'));
+                       }else{
+                              console.log(results);
+                            res.send(results);
+
+                         }
+
+                    //          db.end();
+
+                      });
+                      });
+
+                      app.get("/purchaseInvoiceSummary", function(req, res, next) {
+                        var companyID = req.query.companyID;
+                        var supplierID = req.query.supplierID;
+                        var invoiceNo = req.query.invoiceNo;
+
+
+
+                        console.log(companyID);
+                        var db = mysql.createConnection({
+                        host: process.env.DB_HOST,
+                        user: process.env.DB_USER,
+                        password: process.env.DB_PASSWORD,
+                        database: process.env.DB_NAME,
+                        timezone : "+00:00",
+                      });  // ale/  var userLevel = req.query.userLevel;
+
+                        var sql="SELECT * from invoiceTxn where companyID = '"+companyID+"' and suppCustID = '"+supplierID+"' and invoiceNo = '"+invoiceNo+"' and pur_sal='P'";
+                          // console.log(req.beforeDestroy() {
+                         console.log(sql);
+                          // },);
+                        db.query(sql, function (err, results, fields) {
+                         if(err){
+                           console.log('Error while fetching invoiceTxn ,'+  err);
+                          // results(null,err);
+                          res.send(alert('fail to load Invoice record'));
+                         }else{
+
+
+                                console.log(results);
+                              res.send(results);
+
+                           //results(null,res);
+                           }
+
+
+                    //      db.end();
+
+
+                        });
+                        });
 
                     app.get("/purchaseInvoiceDetail", function(req, res, next) {
                       var companyID = req.query.companyID;
